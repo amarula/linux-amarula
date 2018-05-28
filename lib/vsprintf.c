bfe80ed3d7c7697878c88d8e3cf6a23113690ab1
Tobin C. Harding <me@tobin.cc>
2018-05-28 11:46:42 +1000

    vsprintf: add command line option debug_boot_weak_hash
    Currently printing [hashed] pointers requires enough entropy to be
    available.  Early in the boot sequence this may not be the case
    resulting in a dummy string '(____ptrval____)' being printed.  This
    makes debugging the early boot sequence difficult.  We can relax the
    requirement to use cryptographically secure hashing during debugging.
    This enables debugging while keeping development/production kernel
    behaviour the same.
    
    If new command line option debug_boot_weak_hash is enabled use
    cryptographically insecure hashing and hash pointer value immediately.
    
    Cc: Anna-Maria Gleixner <anna-maria@linutronix.de>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: Randy Dunlap <rdunlap@infradead.org>
    Signed-off-by: Tobin C. Harding <me@tobin.cc>
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>

--- lib/vsprintf.c
+++ lib/vsprintf.c
@@ -1670,6 +1670,20 @@ char *pointer_string(char *buf, char *end, const void *ptr,
 }
 
 static DEFINE_STATIC_KEY_TRUE(not_filled_random_ptr_key);
+
+/* Make pointers available for printing early in the boot sequence. */
+static int debug_boot_weak_hash __ro_after_init;
+EXPORT_SYMBOL(debug_boot_weak_hash);
+
+static int __init debug_boot_weak_hash_enable(char *str)
+{
+	debug_boot_weak_hash = 1;
+	pr_info("debug_boot_weak_hash enabled\n");
+	return 0;
+}
+early_param("debug_boot_weak_hash", debug_boot_weak_hash_enable);
+
+static bool have_filled_random_ptr_key __read_mostly;
 static siphash_key_t ptr_key __read_mostly;
 
 static void enable_ptr_key_workfn(struct work_struct *work)
@@ -1720,6 +1734,12 @@ static char *ptr_to_id(char *buf, char *end, void *ptr, struct printf_spec spec)
 {
 	unsigned long hashval;
 	const int default_width = 2 * sizeof(ptr);
+
+	/* When debugging early boot use non-cryptographically secure hash */
+	if (unlikely(debug_boot_weak_hash)) {
+		hashval = hash_long((unsigned long)ptr, 32);
+		return pointer_string(buf, end, (const void *)hashval, spec);
+	}
 
 	if (static_branch_unlikely(&not_filled_random_ptr_key)) {
 		spec.field_width = default_width;
