9cb9c93b9e08488dfc108b843584b7d1c485c9f0
Jia He <jia.he@hxt-semitech.com>
2018-07-14 11:20:28 +1000

    mm: page_alloc: restore memblock_next_valid_pfn() on arm/arm64
    b92df1de5d28 ("mm: page_alloc: skip over regions of invalid pfns where
    possible") optimized the loop in memmap_init_zone().  But it causes
    possible panic bug.  So Daniel Vacek reverted it later.
    
    But as suggested by Daniel Vacek, it is fine to using memblock to skip
    gaps and find next valid frame with CONFIG_HAVE_ARCH_PFN_VALID.
    
    Daniel said:
    "On arm and arm64, memblock is used by default. But generic version of
    pfn_valid() is based on mem sections and memblock_next_valid_pfn() does
    not always return the next valid one but skips more resulting in some
    valid frames to be skipped (as if they were invalid). And that's why
    kernel was eventually crashing on some !arm machines."
    
    About the performance consideration:
    As said by James in b92df1de5,
    "I have tested this patch on a virtual model of a Samurai CPU
    with a sparse memory map.  The kernel boot time drops from 109 to
    62 seconds."
    
    Thus it would be better if we restore memblock_next_valid_pfn on
    arm/arm64.
    
    Link: http://lkml.kernel.org/r/1530867675-9018-3-git-send-email-hejianet@gmail.com
    Signed-off-by: Jia He <jia.he@hxt-semitech.com>
    Suggested-by: Daniel Vacek <neelx@redhat.com>
    Cc: AKASHI Takahiro <takahiro.akashi@linaro.org>
    Cc: Andrey Ryabinin <aryabinin@virtuozzo.com>
    Cc: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Daniel Jordan <daniel.m.jordan@oracle.com>
    Cc: Eugeniu Rosca <erosca@de.adit-jv.com>
    Cc: Gioh Kim <gi-oh.kim@profitbricks.com>
    Cc: James Morse <james.morse@arm.com>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Kees Cook <keescook@chromium.org>
    Cc: Kemi Wang <kemi.wang@intel.com>
    Cc: Laura Abbott <labbott@redhat.com>
    Cc: Mark Rutland <mark.rutland@arm.com>
    Cc: Mel Gorman <mgorman@suse.de>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: Nikolay Borisov <nborisov@suse.com>
    Cc: Pavel Tatashin <pasha.tatashin@oracle.com>
    Cc: Petr Tesarik <ptesarik@suse.com>
    Cc: Philip Derrin <philip@cog.systems>
    Cc: Russell King <linux@armlinux.org.uk>
    Cc: Steve Capper <steve.capper@arm.com>
    Cc: Vladimir Murzin <vladimir.murzin@arm.com>
    Cc: Vlastimil Babka <vbabka@suse.cz>
    Cc: Wei Yang <richard.weiyang@gmail.com>
    Cc: Will Deacon <will.deacon@arm.com>
    Cc: YASUAKI ISHIMATSU <yasu.isimatu@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>

--- include/linux/mmzone.h
+++ include/linux/mmzone.h
@@ -1241,6 +1241,8 @@ static inline int pfn_valid(unsigned long pfn)
 		return 0;
 	return valid_section(__nr_to_section(pfn_to_section_nr(pfn)));
 }
+
+#define next_valid_pfn(pfn)	(pfn + 1)
 #endif
 
 static inline int pfn_present(unsigned long pfn)
@@ -1266,6 +1268,10 @@ static inline int pfn_present(unsigned long pfn)
 #endif
 
 #define early_pfn_valid(pfn)	pfn_valid(pfn)
+#ifdef CONFIG_HAVE_MEMBLOCK_PFN_VALID
+extern ulong memblock_next_valid_pfn(ulong pfn);
+#define next_valid_pfn(pfn)	memblock_next_valid_pfn(pfn)
+#endif
 void sparse_init(void);
 #else
 #define sparse_init()	do {} while (0)
@@ -1285,6 +1291,11 @@ struct mminit_pfnnid_cache {
 
 #ifndef early_pfn_valid
 #define early_pfn_valid(pfn)	(1)
+#endif
+
+/* fallback to default definitions*/
+#ifndef next_valid_pfn
+#define next_valid_pfn(pfn)	(pfn + 1)
 #endif
 
 void memory_present(int nid, unsigned long start, unsigned long end);
