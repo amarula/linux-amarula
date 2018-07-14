5ea00c116f56231d8753fc9a9f3974d3273d5ed4
Andrew Morton <akpm@linux-foundation.org>
2018-07-14 11:20:34 +1000

    mm-sparse-abstract-sparse-buffer-allocations-fix-fix
    s/BUG_ON/WARN_ON/
    
    Cc: Pavel Tatashin <pasha.tatashin@oracle.com>
    Cc: Oscar Salvador <osalvador@techadventures.net>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>

--- mm/sparse.c
+++ mm/sparse.c
@@ -469,7 +469,7 @@ static void *sparsemap_buf_end __meminitdata;
 
 void __init sparse_buffer_init(unsigned long size, int nid)
 {
-	BUG_ON(sparsemap_buf);
+	WARN_ON(sparsemap_buf);	/* forgot to call sparse_buffer_fini()? */
 	sparsemap_buf =
 		memblock_virt_alloc_try_nid_raw(size, PAGE_SIZE,
 						__pa(MAX_DMA_ADDRESS),
