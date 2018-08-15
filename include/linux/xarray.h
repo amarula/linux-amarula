caf30ce2bd033456757e6a882b16bc1b5a97decd
Matthew Wilcox <willy@infradead.org>
2018-08-15 14:13:29 -0400

    xarray: Add range store functionality
    This version of xa_store_range() really only supports load and store.
    Our only user only needs basic load and store functionality, so there's
    no need to do the extra work to support marking and overlapping stores
    correctly yet.
    
    Signed-off-by: Matthew Wilcox <willy@infradead.org>

--- include/linux/xarray.h
+++ include/linux/xarray.h
@@ -275,6 +275,8 @@ void *xa_store(struct xarray *, unsigned long index, void *entry, gfp_t);
 void *xa_cmpxchg(struct xarray *, unsigned long index,
 			void *old, void *entry, gfp_t);
 int xa_reserve(struct xarray *, unsigned long index, gfp_t);
+void *xa_store_range(struct xarray *, unsigned long first, unsigned long last,
+			void *entry, gfp_t);
 int xa_alloc(struct xarray *, u32 *id, u32 max, void *entry, gfp_t);
 bool xa_get_mark(struct xarray *, unsigned long index, xa_mark_t);
 void xa_set_mark(struct xarray *, unsigned long index, xa_mark_t);
