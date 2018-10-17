b3cc0c9928df76b38d936503334948a51914243b
Andrew Morton <akpm@linux-foundation.org>
2018-10-17 11:04:11 +1100

    memory_hotplug-free-pages-as-higher-order-fix
    avoid return of void-returning __free_pages_core(), per Oscar
    
    Cc: Arun KS <arunks@codeaurora.org>
    Cc: Oscar Salvador <osalvador@suse.de>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -1337,7 +1337,7 @@ void __init __free_pages_bootmem(struct page *page, unsigned long pfn,
 {
 	if (early_page_uninitialised(pfn))
 		return;
-	return __free_pages_core(page, order);
+	__free_pages_core(page, order);
 }
 
 /*
