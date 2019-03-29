eab126b4fe7e6a8d8e2d0daeaec1f9db1ed9e683
Jagan Teki <jagan@amarulasolutions.com>
2019-03-29 16:00:20 +0530

    DEBUG: for verbose prints on dsi controller.
    Signed-off-by: Jagan Teki <jagan@amarulasolutions.com>

--- drivers/gpu/drm/sun4i/sun4i_dotclock.c
+++ drivers/gpu/drm/sun4i/sun4i_dotclock.c
@@ -68,6 +68,7 @@ static unsigned long sun4i_dclk_recalc_rate(struct clk_hw *hw,
 	if (!val)
 		val = 1;
 
+	printk("%s: val = %d, rate = %d\n", __func__, val, (parent_rate / val));
 	return parent_rate / val;
 }
 
@@ -80,6 +81,7 @@ static long sun4i_dclk_round_rate(struct clk_hw *hw, unsigned long rate,
 	u8 best_div = 1;
 	int i;
 
+	printk("%s: min_div = %d max_div = %d, rate = %d\n", __func__, tcon->dclk_min_div, tcon->dclk_max_div, rate);
 	for (i = tcon->dclk_min_div; i <= tcon->dclk_max_div; i++) {
 		u64 ideal = (u64)rate * i;
 		unsigned long rounded;
@@ -97,6 +99,7 @@ static long sun4i_dclk_round_rate(struct clk_hw *hw, unsigned long rate,
 		rounded = clk_hw_round_rate(clk_hw_get_parent(hw),
 					    ideal);
 
+		printk("ideal = %d, rounded = %d\n", ideal, rounded);
 		if (rounded == ideal) {
 			best_parent = rounded;
 			best_div = i;
@@ -113,6 +116,7 @@ static long sun4i_dclk_round_rate(struct clk_hw *hw, unsigned long rate,
 out:
 	*parent_rate = best_parent;
 
+	printk("%s: div = %d rate = %d\n", __func__, best_div, (best_parent / best_div));
 	return best_parent / best_div;
 }
 
@@ -122,6 +126,7 @@ static int sun4i_dclk_set_rate(struct clk_hw *hw, unsigned long rate,
 	struct sun4i_dclk *dclk = hw_to_dclk(hw);
 	u8 div = parent_rate / rate;
 
+	printk("%s div %d\n", __func__, div);
 	return regmap_update_bits(dclk->regmap, SUN4I_TCON0_DCLK_REG,
 				  GENMASK(6, 0), div);
 }
