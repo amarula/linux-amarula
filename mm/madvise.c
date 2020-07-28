d08f4982d52bcab427a7a8b9d3819bd5431224a0
Naoya Horiguchi <n-horiguchi@ah.jp.nec.com>
2020-07-28 11:28:46 +1000

    mm,hwpoison: remove flag argument from soft offline functions
    The argument @flag no longer affects the behavior of soft_offline_page()
    and its variants, so let's remove them.
    
    Link: http://lkml.kernel.org/r/20200716123810.25292-10-osalvador@suse.de
    Signed-off-by: Naoya Horiguchi <n-horiguchi@ah.jp.nec.com>
    Signed-off-by: Oscar Salvador <osalvador@suse.com>
    Cc: Aneesh Kumar K.V <aneesh.kumar@linux.vnet.ibm.com>
    Cc: Dave Hansen <dave.hansen@intel.com>
    Cc: David Hildenbrand <david@redhat.com>
    Cc: Dmitry Yakunin <zeil@yandex-team.ru>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: Mike Kravetz <mike.kravetz@oracle.com>
    Cc: Naoya Horiguchi <naoya.horiguchi@nec.com>
    Cc: Tony Luck <tony.luck@intel.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>

--- mm/madvise.c
+++ mm/madvise.c
@@ -910,7 +910,7 @@ static int madvise_inject_error(int behavior,
 			pr_info("Soft offlining pfn %#lx at process virtual address %#lx\n",
 				 pfn, start);
 
-			ret = soft_offline_page(pfn, 0);
+			ret = soft_offline_page(pfn);
 		} else {
 			pr_info("Injecting memory failure for pfn %#lx at process virtual address %#lx\n",
 				 pfn, start);
