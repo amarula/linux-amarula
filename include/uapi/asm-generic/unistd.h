6e414d2d06702ca88752a50f606918cf3a404e5f
Andrew Morton <akpm@linux-foundation.org>
2020-07-28 11:29:27 +1000

    mm-madvise-introduce-process_madvise-syscall-an-external-memory-hinting-api-fix-2
    fix include/uapi/asm-generic/unistd.h whoops
    
    Cc: Alexander Duyck <alexander.h.duyck@linux.intel.com>
    Cc: Brian Geffon <bgeffon@google.com>
    Cc: Christian Brauner <christian@brauner.io>
    Cc: Christian Brauner <christian.brauner@ubuntu.com>
    Cc: Daniel Colascione <dancol@google.com>
    Cc: David Rientjes <rientjes@google.com>
    Cc: Jann Horn <jannh@google.com>
    Cc: Jens Axboe <axboe@kernel.dk>
    Cc: Joel Fernandes <joel@joelfernandes.org>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Cc: John Dias <joaodias@google.com>
    Cc: Kirill Tkhai <ktkhai@virtuozzo.com>
    Cc: <linux-man@vger.kernel.org>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: Minchan Kim <minchan@kernel.org>
    Cc: Oleksandr Natalenko <oleksandr@redhat.com>
    Cc: Sandeep Patil <sspatil@google.com>
    Cc: SeongJae Park <sj38.park@gmail.com>
    Cc: SeongJae Park <sjpark@amazon.de>
    Cc: Shakeel Butt <shakeelb@google.com>
    Cc: Sonny Rao <sonnyrao@google.com>
    Cc: Suren Baghdasaryan <surenb@google.com>
    Cc: Tim Murray <timmurray@google.com>
    Cc: Vlastimil Babka <vbabka@suse.cz>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>

--- include/uapi/asm-generic/unistd.h
+++ include/uapi/asm-generic/unistd.h
@@ -863,11 +863,11 @@ __SYSCALL(__NR_faccessat2, sys_faccessat2)
 __SYSCALL(__NR_watch_mount, sys_watch_mount)
 #define __NR_fsinfo 441
 __SYSCALL(__NR_fsinfo, sys_fsinfo)
-#define __NR_fsinfo 443
+#define __NR_process_madvise 442
 __SC_COMP(__NR_process_madvise, sys_process_madvise, compat_sys_process_madvise)
 
 #undef __NR_syscalls
-#define __NR_syscalls 444
+#define __NR_syscalls 443
 
 /*
  * 32 bit systems traditionally used different
